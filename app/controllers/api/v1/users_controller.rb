83b22e2ab900e21656066e33336634d26ff3175c
wbdana <william.b.dana@gmail.com>
2017-09-01 11:31:47 -0400

    Add initial users_controller methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    @user.password = params[:password]
+    if @user.save
+      render json: {status: 200}
+    else
+      render json: {messages: @user.errors.full_messages, status: 400}
+    end
+  end
+
+  def cu
+    render json: current_user
+  end
+
+  def show
+    render json: current_user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :profile_image_url)
+  end
+
 end
