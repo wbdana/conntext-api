05fb20164bd00bee9ea20289f3cb423c2aede7c1
wbdana <william.b.dana@gmail.com>
2017-09-01 10:51:05 -0400

    Create Users, begin Knock integration, routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
