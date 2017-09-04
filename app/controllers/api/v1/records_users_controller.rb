217963cf60bbf5e8db181cd5caa438d0297ecc00
wbdana <william.b.dana@gmail.com>
2017-09-04 19:47:36 -0400

    Update records_users create method

--- app/controllers/api/v1/records_users_controller.rb
+++ app/controllers/api/v1/records_users_controller.rb
@@ -2,13 +2,14 @@ class Api::V1::RecordsUsersController < ApplicationController
   # before_action :authenticate_user
 
   def create
-    @user = current_user
-    @record = Record.find_by(id: params[:id])
+    @user = User.find_by(email: params[:user_email])
+    @record = Record.find(params[:file_id])
     if @record
-      @records_user = RecordsUser.new(user_id: @user.id, @record_id: @record.id)
-      if @records_user.save
-        # WEBSOCKET BROADCAST FOR COLLABORATOR ADDITION
-      end
+      @records_user = RecordsUser.new(user_id: @user.id, record_id: @record.id)
+    end
+    if @records_user.save
+      # WEBSOCKET BROADCAST FOR COLLABORATOR ADDITION
+      render json: {status: 200}
     end
   end
 
