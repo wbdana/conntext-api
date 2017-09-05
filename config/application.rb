fb2ef58432da8532ce64f9a6dc980c1c1fa5ec80
wbdana <william.b.dana@gmail.com>
2017-09-05 16:45:06 -0400

    Update Rack::CORS middleware in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module ConntextApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :delete]
       end
     end
 
