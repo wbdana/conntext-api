aef9939986067c6434cfef31ca0420607900dde7
wbdana <william.b.dana@gmail.com>
2017-09-10 17:17:33 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email: 'wbdana', password: 'test', profile_image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Shannon_and_Chesapeake_%28NYPL_Hades-118375-54321%29.jpg/300px-Shannon_and_Chesapeake_%28NYPL_Hades-118375-54321%29.jpg')
+
+User.create(email: 'linda', password: 'test', profile_image_url: 'https://www.petfinder.com/wp-content/uploads/2012/11/152964589-welcome-home-new-cat-632x475.jpg')
+
 User.create(email: 'bob', password: 'test', profile_image_url: 'http://www.britishbattles.com/wp-content/uploads/2017/02/montague-dawson-the-battle-of-trafalgar_.jpg')
+
+User.create(email: 'penny', password: 'test', profile_image_url: 'https://i.pinimg.com/736x/5a/37/68/5a37682af47e1c3997939d9031a47fd1--baby-horses-appaloosa-horses.jpg')
+
+User.create(email: 'tommy', password: 'test', profile_image_url: 'https://pbs.twimg.com/profile_images/378800000661871759/78d081081904390ac720a8741724f17a.jpeg')
+
+User.create(email: 'henry', password: 'test', profile_image_url: 'https://i.pinimg.com/736x/ca/fa/13/cafa13655e0d2a9f16ad9ae004cc6d26--funny-comics-funny-cartoons.jpg')
 
 User.create(email: 'jim', password: 'test', profile_image_url: 'http://www.britishbattles.com/wp-content/uploads/2017/02/montague-dawson-the-battle-of-trafalgar_.jpg')
 
-User.create(email: 'w', password: 'test', profile_image_url: 'http://www.britishbattles.com/wp-content/uploads/2017/02/montague-dawson-the-battle-of-trafalgar_.jpg')
-
-Record.create(name: 'test', language: 'javascript', content: 'First file! It\'s awesome', owner_id: 1)
-
-Record.create(name: 'test', language: 'ruby', content: 'Second file! It\'s also awesome', owner_id: 2)
-
-RecordsUser.create(user_id: 1, record_id: 1)
-RecordsUser.create(user_id: 1, record_id: 2)
-RecordsUser.create(user_id: 2, record_id: 2)
+User.create(email: 'biscuit', password: 'test', profile_image_url: 'https://pbs.twimg.com/profile_images/811632075887538177/-FZ21Kml_400x400.jpg')
