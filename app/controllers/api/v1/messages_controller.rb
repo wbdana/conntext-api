2aa8a303c72cb5072543734a9557174f01e9c75a
wbdana <william.b.dana@gmail.com>
2017-09-11 11:02:14 -0400

    Add partners to broadcast in messages#create

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::MessagesController < ApplicationController
     @message = Message.new(record_id: params[:record_id], content: params[:content], username: @user.email)
     if @message.save
       render json: {record: @record, messages: @record.messages, status: 200}
-      RecordChannel.broadcast_to(@record, {record: @record, messages: @record.messages})
+      RecordChannel.broadcast_to(@record, {record: @record, messages: @record.messages, partners: @record.users})
     else
       render json: {messages: @message.errors.full_messages, status: 400}
     end
