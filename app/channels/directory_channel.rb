482106481631f36e1963980d4339b4b87c8e1ae0
wbdana <william.b.dana@gmail.com>
2017-09-12 11:21:58 -0400

    Fix

--- app/channels/directory_channel.rb
+++ app/channels/directory_channel.rb
@@ -5,7 +5,8 @@ class DirectoryChannel < ApplicationCable::Channel
   end
 
   def received(data)
-    @users = User.all.map{|user| {id: user.id, email: user.email, profile_image_url: user.profile_image_url}}    DirectoryChannel.broadcast_to("directory", {users: @user})
+    @users = User.all.map{|user| {id: user.id, email: user.email, profile_image_url: user.profile_image_url}}
+    DirectoryChannel.broadcast_to("directory", {users: @user})
   end
 
   def unsubscribed
