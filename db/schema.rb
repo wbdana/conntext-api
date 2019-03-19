71f9ecd3bc2e0cc0305a75e77f8c0e9f70b218fd
wbdana <william.b.dana@gmail.com>
2019-03-19 18:04:47 -0400

    Updates for continued development

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901150137) do
+ActiveRecord::Schema.define(version: 20170907134244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "record_id"
+    t.string "username"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "records", force: :cascade do |t|
     t.string "name"
